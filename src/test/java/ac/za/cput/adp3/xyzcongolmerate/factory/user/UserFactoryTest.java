fa6160ea5eb1ad283aea63bc78430a34868acce6
Rowan Kirchner <215071921@mycput.ac.za>
2019-09-03 11:49:06 +0200

    Second commit

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserFactoryTest.java
@@ -1,5 +1,6 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.User;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +8,18 @@ import static org.junit.Assert.*;
 public class UserFactoryTest {
 
     @Test
-    public void buildUser() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void buildUser()
+    {
+        User user1 = UserFactory.buildUser("rowanKirchner@gmail.com", "Rowan", "Kirchner");
+        User user2 = UserFactory.buildUser("matthewPearce@gmail.com", "Matthew", "Jumarak");
+
+        assertNotNull(user1);
+        assertNotNull(user2);
+        assertEquals(user1.getFirstName(), "Rowan");
+        assertEquals(user2.getUserEmail(), "matthewPearce@gmail.com");
+        assertTrue(user1 instanceof User);
+        assertNotEquals(user1.getFirstName(), user2.getLastName());
+
+        System.out.println("Two different users: " +user1.toString() + " and " +user2.toString());
     }
 }
--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserRoleFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserRoleFactoryTest.java
@@ -1,6 +1,9 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserRole;
 import org.junit.Test;
+
+import java.util.UUID;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +11,17 @@ public class UserRoleFactoryTest {
 
     @Test
     public void buildUserRole() {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        UserRole role1 = UserRoleFactory.buildUserRole(UUID.randomUUID().toString(),"rowankirchner@kineticskunk.com", "1");
+        UserRole role2 = UserRoleFactory.buildUserRole(UUID.randomUUID().toString(),"ethanstoffels@westerncape.gov", "2");
+
+        assertNotNull(role1);
+        assertNotNull(role2);
+        assertEquals(role1.getRoleId(), "1");
+        assertEquals(role2.getUserEmail(), "ethanstoffels@westerncape.gov");
+        assertTrue(role1 instanceof UserRole);
+        assertNotEquals(role1.getUserEmail(), role2.getUserEmail());
+
+        System.out.println("Two different users: " +role1.toString() + " and " +role1.toString());
     }
 }
